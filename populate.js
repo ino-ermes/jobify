29426d1a1ce6af406d6332ad46c07e29e50a4ed9
ino ermes <hikigayahachiman.akame.kill@gmail.com>
2023-07-08 13:33:11 +0700

    populate mock data

---
+++ populate.js
@@ -0,0 +1,21 @@
+import {readFile} from 'fs/promises';
+import Job from './models/Job.js';
+import connectDB from './db/connect.js';
+import dotenv from 'dotenv';
+
+(async () => {
+    dotenv.config();
+    try {
+        await connectDB(process.env.MONGO_URL);
+        await Job.deleteMany();
+        let jobs = JSON.parse(await readFile(new URL('./MOCK_DATA.json', import.meta.url)));
+        await Job.create(jobs);
+        console.log('success');
+        process.exit(0);
+    } catch(error) {
+        console.log(error);
+        process.exit(1);
+    }
+
+    
+})();
